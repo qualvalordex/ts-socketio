8a4334934425f2d7df8c64854a8176c13a52f37f
Paulo H Rodrigues <qualvalordex@gmail.com>
2022-01-08 20:07:46 -0300

    :sparkles: implement simple socket.io chat

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import express, { Request, Response } from 'express';
 const app = express();
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.get('/', (req: Request, res: Response) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
--- src/socket.ts
+++ src/socket.ts
@@ -1,5 +1,7 @@
 import { Server } from 'socket.io';
 
+const messages: Array<any> = [];
+const users: Array<any> = [];
 
 function handleSocketIO(server: any) {
     const io = new Server(server);
@@ -7,8 +9,21 @@ function handleSocketIO(server: any) {
     io.on('connection', (socket: any) => {
         console.log(`User ${socket.id} connected.`);
 
+        users.push(socket.id);
+
+        socket.emit('handshake', socket.id, messages, users);
+        socket.broadcast.emit('userConnected', socket.id);
+
+        socket.on('message', (message: any) => {
+            messages.push(message);
+            socket.broadcast.emit('message', message);
+        });
+
         socket.on('disconnect', () => {
             console.log(`User ${socket.id} disconnected.`);
+
+            users.splice(users.indexOf(socket.id), 1);
+            socket.broadcast.emit('userDisconnected', socket.id);
         });
     });
 }
